707eceaa0e057cb829910ed9029cb0443a0a0127
Mateo <mateoarcidiacono@gmail.com>
2021-09-09 20:13:52 -0300

    Datos de cursada agregados

--- controlador.py
+++ controlador.py
@@ -1,3 +1,9 @@
+# Alumno: Mateo Arcidiacono #
+# Curso: 999185859 #
+# Profesor: MBA ING. Juan Barreto #
+# Año: 2021 #
+
+
 # ------------------------- Imports ------------------------- #
 import vista
 
