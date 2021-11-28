3111154a3f93a5f2521296433d57fc40352c23a5
Mateo <mateoarcidiacono@gmail.com>
2021-11-28 10:56:21 -0300

    logs.py modificado

--- logs.py
+++ logs.py
@@ -4,7 +4,7 @@ import datetime
 def log(func):
     def wrapper(*args, **kwargs):
         with open("logs.txt", "a") as f:
-            f.write("Se llamo a la funcion " + " ".join([str(arg) for arg in args]) + " el " + str(datetime.datetime.now()) + "\n")
+            f.write("Se llamo a una funcion " + "".join([str(arg) for arg in args]) + " el " + str(datetime.datetime.now()) + "\n")
         val = func(*args, **kwargs)
         return val
 
